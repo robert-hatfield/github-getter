008ee971a4c003115853477d0e94c10f147d549e
robert-hatfield <robert-hatfield@users.noreply.github.com>
2017-04-05 17:42:21 -0700

    Add a button to return to repo list

--- GitHub Getter/GitHub Getter/RepoDetailViewController.swift
+++ GitHub Getter/GitHub Getter/RepoDetailViewController.swift
@@ -14,6 +14,23 @@ class RepoDetailViewController: UIViewController {
         super.viewDidLoad()
 
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        super.prepare(for: segue, sender: sender)
+        if segue.identifier == RepoViewController.identifier {
+            segue.destination.transitioningDelegate = self
+        }
+    }
 
+    @IBAction func returnToRepoList(_ sender: Any) {
+        self.performSegue(withIdentifier: RepoViewController.identifier, sender: nil)
+    }
 
+}
+
+//MARK: Controller transitioning delegate extension
+extension RepoDetailViewController: UIViewControllerTransitioningDelegate {
+    func animationController(forPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        return CustomTransition(duration: 1.0)
+    }
 }
