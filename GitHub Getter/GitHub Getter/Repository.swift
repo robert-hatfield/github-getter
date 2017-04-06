d3d56e8a3fd1bf9b7dbf14fc847d7b6cb1e49523
robert-hatfield <robert-hatfield@users.noreply.github.com>
2017-04-05 18:44:43 -0700

    Add additional properties to Repository class Added number of stars, isFork, and createdAt

--- GitHub Getter/GitHub Getter/Repository.swift
+++ GitHub Getter/GitHub Getter/Repository.swift
@@ -13,14 +13,28 @@ class Repository {
     let name: String
     let description: String?
     let language: String?
+    let stars: Int
+    let isFork: Bool
+    let createdAt: Date
 //    let languagesUrl: URL?
     
     init?(json: [String: Any]) {
-        if let name = json["name"] as? String {
-            self.name = name
-            self.description = json["description"] as? String ?? nil
-            self.language = json["language"] as? String ?? nil
-        } else { return nil }
+        guard let name = json["name"] as? String,
+            let createdAtString = json["created_at"] as? String else {
+            return nil
+        }
+        self.name = name
+        
+        let dateFormatter = ISO8601DateFormatter()
+        guard let createdAtDate = dateFormatter.date(from: createdAtString) else { return nil }
+        self.createdAt = createdAtDate
+        
+        self.description = json["description"] as? String ?? nil
+        self.language = json["language"] as? String ?? nil
+        self.stars = json["stargazers_count"] as? Int ?? 0
+        self.isFork = json["fork"] as? Bool ?? false
+        
+
         
     }
     
